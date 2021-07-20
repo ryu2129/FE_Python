5d02f3b159de8d779522c94c693f146706beb328
ryu2129 <ryu212920@gmail.com>
2021-07-21 01:16:45 +0900

    1-1

---
+++ README.md
@@ -0,0 +1,3 @@
+# 基本情報技術者試験対策 - Python
+
+## Jupyter Notebookを利用しての学習をしております
--- test.py
+++ test.py
@@ -19,9 +19,116 @@ a = 1
 a = 'Hello'
 
 
-# In[4]:
+# In[5]:
 
 
 a = 2 *3
 b = 'World' * 3
+print(a, b)
+
+
+# In[7]:
+
+
+a=1
+a= a+1
+a
+
+
+# In[8]:
+
+
+a+=1
+a
+
+
+# In[9]:
+
+
+b = 'Hello'
+b = b + 'World'
+b
+
+
+# In[10]:
+
+
+b +='World'
+b
+
+
+# In[12]:
+
+
+print('Hello')
+
+
+# In[14]:
+
+
+print(a)
+
+
+# In[18]:
+
+
+z = input()
+print(z)
+
+
+# In[23]:
+
+
+if a == 1:
+    print('OK')
+    print(a)
+#comment
+
+
+# In[24]:
+
+
+3 + 2 **4
+
+
+# In[25]:
+
+
+a = 9
+b = 4
+
+c = a / b
+print(c)
+
+
+# In[26]:
+
+
+a= 'Hello'
+a + 'World'
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
