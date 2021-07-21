69802c51054df67e7d27ae3d9295fa71b9c129ac
ryu2129 <ryu212920@gmail.com>
2021-07-21 18:48:24 +0900

    finish:1-2

--- test.py
+++ test.py
@@ -109,6 +109,157 @@ a + 'World'
 print(a)
 
 
+# In[28]:
+
+
+string = 'Hello, World!!'
+string.lower()
+
+
+# In[29]:
+
+
+squares = [1,4,9,16,25,36]
+squares[2:5]
+
+
+# In[31]:
+
+
+numbers = (1,2,3,4,5,6)
+numbers[3] = 2
+
+
+# In[34]:
+
+
+tel = {'Mary': 2048, 'John':4096}
+tel['Mary']
+tel['John'] = 8192
+tel
+
+
+# In[36]:
+
+
+basket = {'Apple', 'Melon', 'Apple', 'Water'}
+basket
+
+
+# In[38]:
+
+
+lista = [1,2,5,4,3]
+lista.remove(2)
+lista
+
+
+# In[41]:
+
+
+lista = [1,2,3,4,5]
+del lista[2]
+del lista[1:3]
+lista
+
+
+# In[43]:
+
+
+fruits = ['Apple', 'Melon', 'Strawberry']
+'Apple' in fruits
+'Pumpkin' in fruits
+
+
+# In[46]:
+
+
+stack = [1,2,3]
+stack.append(4)
+stack
+stack.pop()
+stack
+
+
+# In[48]:
+
+
+x = ['a', 'b', 'c', 'd', 'e']
+print(x[:-3])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
 # In[ ]:
 
 
