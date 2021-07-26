2abb3a0df4855a17880bd12e73380f70df55d471
ryu2129 <ryu212920@gmail.com>
2021-07-26 17:16:24 +0900

    finish:5-1

---
+++ 5/5-1.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+x = np.arange(-5, 6,1)
+print(x)
+
+
+# In[6]:
+
+
+y = x ** 2
+print(y)
+
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[16]:
+
+
+plt.plot(x, y)
+plt.show()
+
+
+# In[18]:
+
+
+import pandas as pd
+
+
+# In[19]:
+
+
+df = pd.read_csv('FE2019a_distribution.csv', index_col=0, parse_dates=['Score'])
+
+
+# In[20]:
+
+
+print(df)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
